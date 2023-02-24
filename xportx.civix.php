ddc03b92fc3061f1d12ce576b32250f122a9b400
B. Endres <endres@systopia.de>
2023-02-24 14:48:52 +0100

    update civix

--- xportx.civix.php
+++ xportx.civix.php
@@ -122,7 +122,36 @@ function _xportx_civix_civicrm_config(&$config = NULL) {
  */
 function _xportx_civix_civicrm_install() {
   _xportx_civix_civicrm_config();
+  if ($upgrader = _xportx_civix_upgrader()) {
+    $upgrader->onInstall();
+  }
   _xportx_civix_mixin_polyfill();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function _xportx_civix_civicrm_postInstall() {
+  _xportx_civix_civicrm_config();
+  if ($upgrader = _xportx_civix_upgrader()) {
+    if (is_callable([$upgrader, 'onPostInstall'])) {
+      $upgrader->onPostInstall();
+    }
+  }
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function _xportx_civix_civicrm_uninstall(): void {
+  _xportx_civix_civicrm_config();
+  if ($upgrader = _xportx_civix_upgrader()) {
+    $upgrader->onUninstall();
+  }
 }
 
 /**
@@ -132,7 +161,57 @@ function _xportx_civix_civicrm_install() {
  */
 function _xportx_civix_civicrm_enable(): void {
   _xportx_civix_civicrm_config();
+  if ($upgrader = _xportx_civix_upgrader()) {
+    if (is_callable([$upgrader, 'onEnable'])) {
+      $upgrader->onEnable();
+    }
+  }
   _xportx_civix_mixin_polyfill();
+}
+
+/**
+ * (Delegated) Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ * @return mixed
+ */
+function _xportx_civix_civicrm_disable(): void {
+  _xportx_civix_civicrm_config();
+  if ($upgrader = _xportx_civix_upgrader()) {
+    if (is_callable([$upgrader, 'onDisable'])) {
+      $upgrader->onDisable();
+    }
+  }
+}
+
+/**
+ * (Delegated) Implements hook_civicrm_upgrade().
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed
+ *   based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *   for 'enqueue', returns void
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function _xportx_civix_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  if ($upgrader = _xportx_civix_upgrader()) {
+    return $upgrader->onUpgrade($op, $queue);
+  }
+}
+
+/**
+ * @return CRM_xportx_Upgrader
+ */
+function _xportx_civix_upgrader() {
+  if (!file_exists(__DIR__ . '/CRM/xportx/Upgrader.php')) {
+    return NULL;
+  }
+  else {
+    return CRM_xportx_Upgrader_Base::instance();
+  }
 }
 
 /**
